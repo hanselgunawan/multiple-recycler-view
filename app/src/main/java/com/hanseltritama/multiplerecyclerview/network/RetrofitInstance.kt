0451b0526ad197e0d742e2314c163c02331adae5
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2021-06-02 01:02:21 -0700

    added dependencies

---
+++ app/src/main/java/com/hanseltritama/multiplerecyclerview/network/RetrofitInstance.kt
@@ -0,0 +1,10 @@
+package com.hanseltritama.multiplerecyclerview.network
+
+object RetrofitInstance {
+
+    private const val BASE_URL = "https://rickandmortyapi.com/api/"
+
+    fun getInstance() {
+
+    }
+}
